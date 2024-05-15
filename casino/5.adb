bd5a3f1d83fc5734b58df1c238a7f8a4562d3583
Jamal Mammadov <91668167+jamuall@users.noreply.github.com>
2024-05-15 02:15:58 +0200

    Create 5.adb

---
+++ casino/5.adb
@@ -0,0 +1,168 @@
+with Ada.Text_IO,Ada.Numerics.Float_Random, Ada.Numerics.Discrete_Random, Ada.Real_Time;
+use Ada.Text_IO,Ada.Numerics.Float_Random, Ada.Real_Time;
+
+
+-- Time
+-- Time_span
+
+procedure five is
+   type PStr is access String;
+
+   package randint is new Ada.Numerics.Discrete_Random(Natural);
+
+   iR : randint.Generator;
+
+   protected MyG is
+      procedure Init;
+      function Give return Duration;
+   private
+      G : Generator;
+   end MyG;
+
+   
+   
+   protected body MyG is 
+      procedure Init is
+      begin
+         Reset(G);
+      end;
+
+      function Give return Duration is
+      begin
+         return Duration(Random(G));
+      end Give;
+   end MyG;
+   
+
+   protected Printer is
+      procedure Print(S : String := "");
+   end Printer;
+
+   protected body Printer is
+      procedure Print(S : String := "") is
+      begin
+         Put_Line(S);
+      end Print;
+
+   end Printer;
+   
+   protected Manager is
+      procedure AddPlayer(v : Integer);
+      procedure AddPoint(v : Integer);
+      procedure Print;
+   private
+      totalP : Integer := 0;
+      totalPo: Integer := 0;
+   end Manager;
+
+   protected body Manager is
+      procedure AddPlayer(v : Integer) is
+      begin 
+         totalP := totalP + v;
+      end AddPlayer;
+
+      procedure AddPoint(v : Integer) is
+      begin 
+         totalPo := totalPo + v;
+      end AddPoint;
+
+      procedure Print is
+      begin
+         Printer.Print("Players from all Casinos : " & totalP'Image & " Points from all Players all Casinos : " & totalPo'Image);
+      end Print;
+   end Manager;
+   
+   task type Casino is
+      entry Enter;
+   end Casino;
+   task body Casino is
+      capacity : Natural := 0;
+      open : Boolean := True;
+   begin
+      delay MyG.Give;
+      Printer.Print("Casion: Opened");
+      while open loop
+         select
+            when capacity < 5 =>
+            accept Enter do
+               Printer.Print("Casino: a player got in");
+               capacity := capacity + 1;
+            end Enter;
+         or
+             delay until Clock + Seconds(15);
+            open := False;
+         end select;
+      end loop;
+      Printer.Print("Casino: finished");
+      exception
+      when Tasking_Error => Printer.Print("Casion f");
+      when others => Printer.Print("casion ff");
+   end Casino; 
+
+   type PCasino is access Casino;
+   Casinos : array(1..3) of PCasino;
+
+   task type Player(name : PStr);
+   task body Player is
+      trial : Natural := 0;
+      played : Boolean := False;
+      decide : Boolean := True;
+      point : Natural := 0;
+      ind : Positive := 1;
+   begin
+      
+      delay MyG.Give;
+      ind := (randint.Random(iR) rem 3) + 1;
+      Printer.Print("Player"& name.all &" arrives");
+      while not played and trial < 2 and decide loop 
+         select
+               Casinos(ind).Enter;
+               played := True;
+               Printer.Print("Player" & name.all &": got in" & ind'Image);
+               
+               Manager.AddPlayer(1);
+               
+               point := ( randint.Random(iR) rem 1001 );
+               Printer.Print(name.all & ", he got points:" & point'Image);
+               Manager.AddPoint(point);
+         or
+            delay 1.0;
+            trial := trial + 1;
+            Printer.Print("player:" & name.all &" try again");
+            if MyG.Give < 0.5 then
+               decide := True;
+            else 
+               decide := False;
+            end if;
+         end select;
+      end loop;
+      if not played then
+         Printer.Print(name.all & " did not play");
+      else
+         Printer.Print("player:"&name.all &" finished");
+      end if;
+   exception
+      when Tasking_Error => Printer.Print("Player f");
+      when others => Printer.Print("player ff");
+   end Player;
+
+   type PPlayer is access Player;
+   Players : array(1..20) of PPlayer;
+   
+   dur : constant Time_Span := Milliseconds(500);
+   checktime : Time := Clock + dur;
+begin
+   MyG.Init;
+   randint.Reset(iR);
+   Printer.Print("Game started");
+   for I in 1..3 loop
+      Casinos(I) := new Casino;
+   end loop;
+
+   for I in 1..20 loop
+      Players(I) := new Player(new String'(I'Image));
+      delay 0.5;
+   end loop;
+   delay 20.0;
+   Manager.Print;
+end five;
