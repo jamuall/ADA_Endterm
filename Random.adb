1ed3cbac0f9f46ba68dbb7bcc5774fc2f5f028bb
Jamal Mammadov <91668167+jamuall@users.noreply.github.com>
2024-12-11 14:41:29 +0100

    Update Random.adb

--- Random.adb
+++ Random.adb
@@ -2,23 +2,26 @@ with Ada.Numerics.Float_Random;
 use Ada.Numerics.Float_Random;
 
 procedure main 
-  protected Safe_Random is 
-          procedure Reset;
-          function Generate return Float;
-      private
-          RGen : Ada.Numerics.Float_Random.Generator;
-      end Safe_Random;
-      protected body Safe_Random is 
-          procedure Reset is 
-          begin 
-              Ada.Numerics.Float_Random.reset(RGen);
-          end Reset;
-  
-          function Generate return Float is 
-          begin
-              return Ada.Numerics.Float_Random.Random(RGen);
-          end Generate;
-      end Safe_Random;
+  --Protected Safe_Random
+   protected Safe_Random is
+      procedure Init;
+      function Generate return Float;
+   private
+      G: Generator;
+   end Safe_Random;
+   
+   protected body Safe_Random is
+      procedure Init is
+      begin
+         Reset(G);
+      end Init;
+      
+      function Generate return Float is
+      begin
+         --return Random(G) * (4.0 - 0.0) + 0.0;
+          return Random(G) --returns between 0.0 and 1.0;
+      end Generate;
+   end Safe_Random;
 begin
   null;
 end main;
